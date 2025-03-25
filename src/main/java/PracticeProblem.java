e1b437213e9d7c0641dae0d0c98a2835566971bd
lily <lily.you@student.tdsb.on.ca>
2025-03-25 02:47:17 -0400

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,38 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static void selectionSortName(String[]names, int[]ages){
+		for(int i = 0; i < names.length; i++){
+			int smallest = i;
+			for(int j = i+1; j < names.length; j++){
+				if(names[j].compareToIgnoreCase(names[smallest]) < 0){
+					smallest = j;
+				}
+			}
+			String temp = names[smallest];
+			names[smallest] = names[i];
+			names[i] = temp;
+			int agesTemp = ages[smallest];
+			ages[smallest] = ages[i];
+			ages[i] = agesTemp;
+		}
+	}
+
+	public static void selectionSortAge(String[] names, int[] ages){
+		for(int i = 0; i < ages.length; i++){
+			int smallest = i;
+			for(int j = i+1; j < ages.length; j++){
+				if(ages[j] < ages[smallest]){
+					smallest = j;
+				}
+			}
+			int agesTemp = ages[smallest];
+			ages[smallest] = ages[i];
+			ages[i] = agesTemp;
+			String temp = names[smallest];
+			names[smallest] = names[i];
+			names[i] = temp;
+		}
+	}
 
 }
